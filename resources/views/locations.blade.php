1b6071b014b383ab6634c0886eec5f612faffe1e
michaelnabil23 <46572405+michaelnabil23@users.noreply.github.com>
2021-09-05 15:33:40 +0200

    Update locations.blade.php

--- resources/views/locations.blade.php
+++ resources/views/locations.blade.php
@@ -1,138 +1,127 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-        <style>
-            table {
-              font-family: arial, sans-serif;
-              border-collapse: collapse;
-              width: 100%;
-            }
-            
-            td, th {
-              border: 1px solid #dddddd;
-              text-align: left;
-              padding: 8px;
-              text-align: center !important
-            }
-            
-            tr:nth-child(even) {
-              background-color: #dddddd;
-            }
-            </style>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-           <div style="width:80%;margin:auto">
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            text-align: center !important
+        }
 
-                <div style="text-align: center">
-                    <h3>
-                        all locations for provider : 
-                        <b style="color: #000"> {{$provider->user_name}} </b>
-                    </h3>
-                </div>
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
 
-                <table class="table">
+</head>
 
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">latitude</th>
-                            <th scope="col">longitude</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+<body>
+    <div class="flex-center position-ref full-height">
+        <div style="width:80%;margin:auto">
+            <div style="text-align: center">
+                <h3>
+                    All locations for provider :
+                    <b style="color: #000"> {{ $provider->user_name }} </b>
+                </h3>
+            </div>
+            <table class="table">
 
-
-                        @if($provider->locations != null && $provider->locations()->count() > 0)
-
-                            @foreach ($provider->locations as $item)
-                                <tr>
-                                    <th scope="row">{{$x}}</th>
-                                    <td>{{$item->latitude}}</td>
-                                    <td>{{$item->longitude}}</td>
-                                </tr>
-
-                                @php $x = $x + 1; @endphp
-
-                            @endforeach
-
-                        @else 
-                            <tr>
-                                <td colspan="4">
-                                    there no any locations in this provider yet
-                                </td>
-                            </tr>
-                        @endif
-                        
-                    </tbody>
-                </table>
-           </div>
-
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">latitude</th>
+                        <th scope="col">longitude</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($provider->locations as $location)
+                    <tr>
+                        <th scope="row">{{$x}}</th>
+                        <td>{{ $location->latitude }}</td>
+                        <td>{{ $location->longitude }}</td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="4">
+                            there no any locations in this provider yet
+                        </td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
-    </body>
+    </div>
+</body>
 </html>
